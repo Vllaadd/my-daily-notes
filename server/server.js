a1531924a07507bd9808a0628594e05b05059add
Vllaadd <vlladdiimiir@gmail.com>
2023-12-26 05:43:30 -0500

    connected to mongo database

--- server/server.js
+++ server/server.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
+// const Notes = require('./models/Notes');
 
 //MIDDLEWARE
 const app = express();
 app.use(express.json());
 app.use(express.static('public'));
+
+//MONGO DATABASE
+mongoose.connect('mongodb+srv://vladzizic:IWKhFXpvCEkmaAWt@cluster0.7jw4ap3.mongodb.net/?retryWrites=true&w=majority', {
+    // userNewUrlParser: true,
+    // useUnifiedTopology: true
+})
+.then(() => console.log('Connected to DB'))
+.catch(console.error);
 
 
 app.listen(3000, () => console.log('Server started on port 3000'));
